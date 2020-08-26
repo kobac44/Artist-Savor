2af2abd1e817d8417cbf17df5cb1d7eb10caec39
Victor Litzau <victorlitzau@gmail.com>
2020-08-25 21:40:14 -0400

    Create artist-routes.js

---
+++ routes/artist-routes.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const db = require("./models");
+const Artist = require('../models/Artist');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+// Display new artists form
+router.get('/new', (req, res) => res.render('new'));
+
+// Add a artist
+router.post('/new', (req, res) => {
+    let { email, artistName, address, artform, deposit } = req.body;
+    let errors = [];
+
+    // Validate Fields
+    if (!artistName) {
+        errors.push({ text: 'make sure to add your name to join the artist-savor' });
+    }
+    if (!account) {
+        errors.push({ text: 'please submit your artist acct number' });
+    }
+    if (!deposit) {
+        errors.push({ text: 'How much are you depositing today?' });
+    }
+
+
+    // Check for errors
+    if (errors.length > 0) {
+        res.render('add', {
+            errors,
+            artistName,
+            account,
+            deposit
+        });
+    }
+
+
+
+//     // add artist to table
+    Artist.create({
+        artistName,
+        account,
+        deposit
+    })
+        .then(artist => res.redirect('/artists'))
+        .catch(err => res.render('error', { error: err.message }))
+});
+//remember to redirect back to members page
+
+// // find one artist
+router.get('/search', (req, res) => {
+    let { term } = req.query;
+
+
+    term = term.toLowerCase();
+
+    Artists.findOne({ where: { account: { [Op.like]: '%' + term + '%' } } })
+        .then(artists => res.render('artists', { artist }))
+        .catch(err => res.render('error', { error: err }));
+});
+
+module.exports = router;
