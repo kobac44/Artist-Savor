35b01cd87a8b900a4018a5b13db9197df8807a89
Unko <kobie.watkins@gmail.com>
2020-08-27 19:39:02 -0400

    api artist

--- routes/artist-routes.js
+++ routes/artist-routes.js
@@ -9,61 +9,54 @@ const Op = Sequelize.Op;
 router.get('/new', (req, res) => res.render('new'));
 
 // Add a artist
-router.post('/new', (req, res) => {
-    let { artistName, address, artform, deposit } = req.body;
-    let errors = [];
+module.exports = function (app) {
 
-    // Validate Fields
-    if (!artistName) {
-        errors.push({ text: 'make sure to add your name to join the artist-savor' });
-    }
-    if (!address) {
-        errors.push({ text: 'please submit your address' });
-    }
-    if (!artform) {
-        errors.push({ text: 'please submit your type of art or instrument' });
-    }
-    if (!deposit) {
-        errors.push({ text: 'How much are you depositing today?' });
-    }
-
-
-    // Check for errors
-    if (errors.length > 0) {
-        res.render('add', {
-            errors,
-            artistName,
-            address,
-            artform,
-            deposit,
+    app.get('/', (request, response, next) => {
+        response.render('index', {
         });
-    }
-    else {
-        res.redirect("/members");
-    }
+    });
+    app.create("/passport_demo/")
 
+    app.get("/api/artist/:id", (req, res, next) => {
+        db.Artist.findAll({
+            include: [db.passport_demo]
+        }).then(function (dbpassport_demo) {
+            res.json(dbpassport_demo);
+        });
+    });
 
-    // add artist to table
-    Artist.create({
-        artistName,
-        account,
-        deposit
-    })
-        .then(artist => res.redirect('/artists'))
-        .catch(err => res.render('error', { error: err.message }))
-});
-//remember to redirect back to members page
+    app.get("/api/transaction/balance", (req, res, next) => {
+        db.Transaction.sum({
+            where: {
+                balance: req.params.balance
+            },
+            include: [db.Artist]
+        }).then(function (dbpassport_demo) {
+            res.json(dbpassport_demo);
+        });
+    });
 
-// // find one artist
-router.get('/search', (req, res) => {
-    let { term } = req.query;
+    app.put("/api/transaction/deposit", (req, res, next) => {
+        db.Transaction.update({
+            where: {
+                balance: req.body.deposit
+            },
 
+        }).then(function (dbpassport_demo) {
+            res.json(dbpassport_demo);
+        });
+    });
 
-    term = term.toLowerCase();
-
-    Artists.findOne({ where: { account: { [Op.like]: '%' + term + '%' } } })
-        .then(artists => res.render('artists', { artist }))
-        .catch(err => res.render('error', { error: err }));
-});
+    app.put("/api/transaction/withdrawl", (req, res, next) => {
+        db.Transaction.update({
+            where: {
+                balance: req.body.withdrawl
+            },
+            include: [db.transaction]
+        }).then(function (dbpassport_demo) {
+            res.json(dbpassport_demo);
+        });
+    });
+}
 
 module.exports = router;
