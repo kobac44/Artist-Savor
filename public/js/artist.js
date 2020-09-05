07462c0e826a6fdab01c803166e8c0a621978cd3
victorsc45 <victorlitzau@gmail.com>
2020-09-05 16:19:41 -0400

    documentation and css still working on and functionality uptodate

--- public/js/artist.js
+++ public/js/artist.js
@@ -1,37 +1,32 @@
 
 $(document).ready(() => {
-
-
-
-    var ctxOne = $("#pieChart");
-    netDataSet = [];
-
-    var getNetData = function () {
+    // variable of id for pie chart view
+    let ctx = $("#pieChart");
+    //data set for the pie chart to animate
+    artistDataArr = [];
+    //function to load and render pie chart
+    const allArtistTots = function () {
         $.get("/api/user_data").then(function (data) {
             email = data.email;
-            $.get("/api/pay/total/" + email).then(function (datatwo) {
-                netDataSet.push(datatwo[0].tot_amt);
-                // console.log(datatwo[0].tot_amt);
-                $.get("/api/cost/total/" + email).then(function (datathree) {
-                    // console.log(datathree[0].tot_amt);
-                    netDataSet.push(datathree[0].tot_amt);
-                    // console.log(netDataSet)
-                    new Chart(ctxOne, {
+            $.get("/api/pay/total/" + email).then(function (payTotal) {
+                artistDataArr.push(payTotal[0].tot_amt);
+                //get total cost 
+                $.get("/api/cost/total/" + email).then(function (costTotal) {
+                    // push array of total cost
+                    artistDataArr.push(costTotal[0].tot_amt);
+                    // new instantiation of chart(pie)
+                    new Chart(ctx, {
                         type: "pie",
                         data: {
                             labels: ["Artist Savor", "Artist Cost"],
                             datasets: [{
-                                // data: [netDataSet[0], netDataSet[2]],
-                                data: netDataSet,
+                                data: artistDataArr,
                                 backgroundColor: [
                                     "#2ecc71",
                                     "#3498db"],
                             }]
                         },
                         options: {
-                            legend: {
-                                display: false,
-                            },
                             title: {
                                 display: true,
                                 text: 'Artist Savor vs. Artist Cost'
@@ -42,15 +37,19 @@ $(document).ready(() => {
                                         beginAtZero: true
                                     }
                                 }]
+                            },
+                            animation: {
+                                duration: 4000,
                             }
                         }
                     });
-                    $("#artistTotal").text(netDataSet[0] - netDataSet[1]).toFixed(2);
+                    // loading view with the collected data set from pay and cost
+                    $("#artistTotal").text(artistDataArr[0] - artistDataArr[1]);
                 });
             });
         });
     }
-
-    getNetData();
+    //call function for pie chart instantiation
+    allArtistTots();
 
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,8 +4,6 @@ const path = require("path");
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("../models");
 const express = require("express");
-
-
 module.exports = function (app) {
   app.get("/", (req, res) => {
     // If the user already has an account send them to the members page
@@ -13,44 +11,38 @@ module.exports = function (app) {
       res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
-
   });
-
   app.get("/signup", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
-
   });
-
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
-
   });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     db.User.findOne({
+      //search for user by id from user model(table)
       where: {
         id: req.user.id
       },
+      // include both models with user model
       include: [db.Pay, db.Cost],
-
-
     }).then((data) => {
-      console.log(data);
+      //renders index handlebars after auth 
       res.render("index", {
-
+        // data is pulled from the database tables user, pay, and cost
         user: data,
         pay: data.Pays,
         cost: data.Costs
-
       });
     });
   });
